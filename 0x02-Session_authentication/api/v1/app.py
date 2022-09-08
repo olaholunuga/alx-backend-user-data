cd5c41f0c45f7d41283a86438cde29fd91868604
“olaholunuga” <olaholunuga@gmail.com>
2022-09-08 12:31:24 +0100

    copied starter files

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+
+auth_env = getenv("AUTH_TYPE", None)
+app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = Auth() if auth_env == "auth" else None
+if auth_env == "basic_auth":
+    auth = BasicAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before():
+    """ test for authentication
+    """
+    if not auth:
+        return
+    auth_test = auth.require_auth(request.path, [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
+    if not auth_test:
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port, debug=True)
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""auth class module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ method to test if authenticated
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        if path[:-1] in excluded_paths:
+            return False
+        if path + "/" in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ request: flask request object
+        return: None
+        """
+        auth_key = request.headers.get("Authorization")
+        if request is None:
+            return None
+        if auth_key is None:
+            return None
+        return auth_key
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ request: flask request object
+        return: None
+        """
+        return None
