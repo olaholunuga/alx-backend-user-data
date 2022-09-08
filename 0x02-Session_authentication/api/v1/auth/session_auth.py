d98f57c1d900671a4314a16a47d9e0010958796b
“olaholunuga” <olaholunuga@gmail.com>
2022-09-08 22:22:52 +0100

    fix: coding style

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,13 +26,3 @@ class SessionAuth(Auth):
         if not session_id or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
-
-    # def session_cookie(self, request=None) -> str:
-    #     """ return session cookie from request object
-    #     """
-    #     if not request:
-    #         return None
-    #     cookie_id = getenv("SESSION_NAME", None)
-    #     if not cookie_id:
-    #         return None
-    #     return request.cookies.get(cookie_id)
