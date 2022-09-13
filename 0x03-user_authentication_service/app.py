f93706dfbe9a1cf87d823b97c789c6047340d9bd
“olaholunuga” <olaholunuga@gmail.com>
2022-09-13 14:08:49 +0100

    creat: login function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """app module
 """
-from flask import Flask, jsonify, request
+import email
+import re
+from flask import Flask, abort, jsonify, make_response, request, session
 from auth import Auth
 from os import urandom
 
@@ -27,6 +29,21 @@ def creat_user():
             return jsonify({"email": f"{email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def sessions():
+    """ session login method
+    """
+    password = request.form.get("password")
+    email = request.form.get("email")
+    login = AUTH.valid_login(email, password)
+    if login:
+        session_id = AUTH.create_session(email)
+        resp = make_response(jsonify({"email": f"{email}", "message": "logged in"}))
+        resp.set_cookie("session_id", session_id)
+        return resp
+    else:
+        abort(401)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
