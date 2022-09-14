015bbdc163939b6dbae0ef68bfe57041ba546837
“olaholunuga” <olaholunuga@gmail.com>
2022-09-14 11:01:25 +0100

    create: getting user by given session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -65,3 +65,12 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ get the user object of a given session id
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
